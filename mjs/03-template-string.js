81006ce302410359a1f025e9363c78f620637ef5
NguyenLinhtp <nguyenlinhqndn@gmail.com>
2017-09-14 09:29:06 +0700

    Tenplate-string

---
+++ mjs/03-template-string.js
@@ -0,0 +1,21 @@
+/*----------------Template String----------------*/
+var elm = document.getElementById("mContent");
+// elm.style.color = "#5d7fe2";
+// var name_course = "TypeScript";
+// var price = "10";
+// elm.innerHTML = '<div> Course Name: <b>' + name_course + '</b>, price: <b style="color: red"> '+price+' USD</b> </div>';
+
+// /*Using Template String, thay ' => `
+// elm.innerHTML = `<div> Course Name: <b>${name_course}</b>,
+//                     price: <b style="color: red"> ${price} USD</b> 
+//                 </div>`;
+
+let todo = {
+    id: 100,
+    name: "Designer",
+    status: false
+};
+elm.innerHTML = `<div id="todo-id-${todo.id}">
+                    <i class="${todo.status == true ? "hidden" : ""} glyphicon glyphicon-ok"></i>
+                    <span class="name">${todo.name}</span>
+                </div>`;
