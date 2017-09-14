dfc29bbd9607838ab65cd6db90f64380a4e8333a
NguyenLinhtp <nguyenlinhqndn@gmail.com>
2017-09-14 09:59:00 +0700

    For

---
+++ mjs/04-for.js
@@ -0,0 +1,29 @@
+let todoArr = [
+    "Coding",
+    "Study Script",
+    "Designer"
+];
+/* --------For-------- */
+// let lengthArr = todoArr.length;
+// for(let i = 0; i<lengthArr; i++){
+//     console.log(i+": "+ todoArr[i]);
+// }
+/* --------For IN-------- */
+// for(let index in todoArr){
+//     console.log(index+": "+todoArr[index]);
+// }
+/* --------For OF-------- */
+// for(let todo of todoArr){
+//     console.log(todo);
+// }
+
+/* --------For OBJECT-------- */
+let todoObj = {
+    id: 100,
+    name: "Designer",
+    status: false
+};
+for(let index in todoObj){
+    console.log(index+": "+todoObj[index] );
+}
+/* Chú ý: Đối với một đối tượng chúng ta không sử for of */
